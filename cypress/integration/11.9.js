4939f81b40c052144781d83ba36f792037f36da0
laptop <juha.jylhakoski@aalto.fi>
2021-10-31 15:33:06 +0800

    11.9

--- cypress/integration/11.9.js
+++ cypress/integration/11.9.js
@@ -9,7 +9,7 @@ describe('Pokedex', function() {
 
 describe('Pokedex 2', function() {
   it('pokemon page can be opened', function() {
-    cy.visit('http://localhost:5000/pokemon/ivysaur')
+    cy.visit('http://localhost:5000/pokemon/ivysaur', { timeout: 45000 })
     //cy.visit('http://localhost:8081/pokemon/ivysaur')
     cy.contains('ivysaur')
   })
