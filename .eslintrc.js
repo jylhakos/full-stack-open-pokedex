f40ce05c19fe4daf7c5201193c40dc5edcec5246
laptop <juha.jylhakoski@aalto.fi>
2021-10-31 14:06:03 +0800

    11.9

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     "commonjs": true,
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
