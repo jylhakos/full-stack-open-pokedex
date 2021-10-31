6dd5f0130ca7b3a041e7271afebd38e0c55435ec
laptop <juha.jylhakoski@aalto.fi>
2021-10-31 16:12:42 +0800

    11.9

--- cypress/integration/11.9.2.js
+++ cypress/integration/11.9.2.js
@@ -1,10 +1,11 @@
 
 describe('Pokedex 2', function() {
   it('pokemon page can be opened', function() {
-    cy.intercept('/pokemon/ivysaur').as('ivysaur')
+    //cy.intercept('/pokemon/ivysaur').as('ivysaur')
     cy.visit('http://localhost:5000', { timeout: 45000 })
-    cy.wait('@ivysaur')
+    //cy.wait('@ivysaur')
     //cy.visit('http://localhost:8081/pokemon/ivysaur')
+    cy.get('a[href*="/pokemon/ivysaur"]').click()
     cy.contains('ivysaur')
   })
 })
